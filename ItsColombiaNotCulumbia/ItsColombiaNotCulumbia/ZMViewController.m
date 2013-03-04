2e2d088dcb6a3543a2cbf33d4b1f6c1f94ea30e3
Sebastian Romero <bipsa81@gmail.com>
2013-03-03 23:49:24 -0500

    skin
    skin

--- ItsColombiaNotCulumbia/ItsColombiaNotCulumbia/ZMViewController.m
+++ ItsColombiaNotCulumbia/ItsColombiaNotCulumbia/ZMViewController.m
@@ -39,13 +39,29 @@
 }
 
 
+
 -(void) imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
 {
     UIImage * image = [info objectForKey:@"UIImagePickerControllerEditedImage"];
     self.image.contentMode = UIViewContentModeScaleAspectFit;
-    self.image.image = image;
+    self.image.image = [self maskImage:image withMask:[UIImage imageNamed:@"maskPhoto"]];
     [picker dismissViewControllerAnimated:YES completion:nil];
     self.btnDone.hidden = NO;
+}
+
+
+/**
+ * @private
+ * Mask the given image
+ **/
+- (UIImage*) maskImage:(UIImage *)image withMask:(UIImage *)maskImage {
+    CGImageRef maskRef = maskImage.CGImage;
+    CGImageRef mask = CGImageMaskCreate(CGImageGetWidth(maskRef), CGImageGetHeight(maskRef), CGImageGetBitsPerComponent(maskRef), CGImageGetBitsPerPixel(maskRef), CGImageGetBytesPerRow(maskRef), CGImageGetDataProvider(maskRef), NULL, false);
+    CGImageRef maskedImageRef = CGImageCreateWithMask([image CGImage], mask);
+    UIImage *maskedImage = [UIImage imageWithCGImage:maskedImageRef];
+    CGImageRelease(mask);
+    CGImageRelease(maskedImageRef);
+    return maskedImage;
 }
 
 
